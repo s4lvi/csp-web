1fab885c68c53cb4431a15c105fa83fd99e2f866
Jordan Salvi <salvi@salviMBP.local>
2019-11-16 16:49:04 -0500

    wip

--- src/app/portal/dlc-annotate/dlc-annotate.component.ts
+++ src/app/portal/dlc-annotate/dlc-annotate.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-dlc-annotate',
   templateUrl: './dlc-annotate.component.html',
   styleUrls: ['./dlc-annotate.component.css']
 })
-export class DlcAnnotateComponent implements OnInit {
+export class DlcAnnotateComponent implements AfterViewInit {
   
   @ViewChild('canvas', {
     static: false
@@ -24,12 +24,18 @@ export class DlcAnnotateComponent implements OnInit {
   constructor() {
   }
 
-  ngOnInit() { 
+  ngAfterViewInit() { 
+    this.context = (this.canvasRef.nativeElement as HTMLCanvasElement).getContext('2d');
     this.currentLabel = this.labels.length > 0 ? this.labels[0] : {name: "label", color: "red"}; 
   }
 
   annotate(e) {
     console.log(e);
+
+    this.context.fillStyle = "red";//this.currentLabel.color;
+    this.context.beginPath();
+    this.context.arc(e.screenX, e.screenY, 10, 0, 2 * Math.PI);
+    this.context.fill();
   }
 
 }
