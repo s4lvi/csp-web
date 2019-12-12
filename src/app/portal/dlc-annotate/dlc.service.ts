3e9aa3da4d3c3aa32e2c687843ece16716403a5f
s4lvi <salvicomputers@gmail.com>
2019-12-12 04:19:35 -0500

    loading frames works

--- src/app/portal/dlc-annotate/dlc.service.ts
+++ src/app/portal/dlc-annotate/dlc.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpHeaders, HttpClient, HttpResponse } from '@angular/common/http';
 import { PostFramesRequest } from './model/post-frames-request.model';
 import { Observable } from 'rxjs';
+import { AnnotatedFrame } from './dlc-annotate.component';
 
 const httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json', 
@@ -16,9 +17,9 @@ export class DlcService {
         
     }
 
-    getFrames(projectId: string) {
+    getFrames(projectId: string): Observable<AnnotatedFrame[]> {
         //return this.http.get('/dlc/project/' + projectId);
-        return this.http.get('/dlc/project/c41f25f1-4dbf-4ec3-a92c-8e7142764b64');
+        return this.http.get<AnnotatedFrame[]>('http://localhost:8080/dlc/project/c41f25f1-4dbf-4ec3-a92c-8e7142764b64/frames', httpOptions);
     }
 
     postFrames(request: PostFramesRequest): Observable<HttpResponse<String>> {
